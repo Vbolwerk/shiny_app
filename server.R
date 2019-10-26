f73df9977d2e98fb94f62d8fcbeab82cfec5801b
Bolwerk <vbolwerk@deloitte.nl>
2019-10-26 18:49:43 +0200

    Improvements to initial app

--- server.R
+++ server.R
@@ -1,22 +1,27 @@
 library(shiny)
+library(ggplot2)
+
 
 # Load data
-setwd("C:/Users/VBolwerk/Documents/Personal development/R course/part_9/shiny_app/shiny_app")
+setwd("C:/Users/VBolwerk/Documents/sourcetree/shiny_app")
 data <-read.csv("hospital-data.csv")
+drops <- c("Address.2","Address.3")
+data<-data[ , !(names(data) %in% drops)]
 
 shinyServer(function(input, output) {
-  display_table <- reactive({
-    t <- reactive(paste0("data[data$Emergency.services==", input$emergency, "]"))
-    
-    # Read the RCA matrix
-    long_table=data
-    
-    return(long_table)
-  })
+  # Filter data based on selections
+  output$table <- DT::renderDataTable(DT::datatable({
+    data <- data
+    if (input$Hospital.Name != "All") {
+      data <- data[data$Hospital.Name == input$Hospital.Name,]
+    }
+    if (input$Address.1 != "All") {
+      data <- data[data$Address.1 == input$Address.1,]
+    }
+    if (input$Emergency.Services != "All") {
+      data <- data[data$Emergency.Services == input$Emergency.Services,]
+    }
+    data
+  }))
   
-  
-  output$dataTable <- renderDataTable({
-    display_table()
-  })
 })
-
--- ui.R
+++ ui.R
@@ -1,9 +1,35 @@
 library(shiny)
+library(ggplot2)
 
+# Load data
+setwd("C:/Users/VBolwerk/Documents/sourcetree/shiny_app")
+data <-read.csv("hospital-data.csv")
+drops <- c("Address.2","Address.3")
+data<-data[ , !(names(data) %in% drops)]
 shinyUI(fluidPage(
-  headerPanel("Get the hospitals in your state, by Vincent Bolwerk"),
-  mainPanel(
-    h3("Hospitals in your state. Search by state or any other search term!"),
-    dataTableOutput('dataTable')
-  )
+  titlePanel("Get the hospitals in your state, by Vincent Bolwerk"),
+  
+  # Create a new Row in the UI for selectInputs
+  fluidRow(
+    column(4,
+           selectInput("Hospital.Name",
+                       "Hospital Name:",
+                       c("All",
+                         unique(as.character(data$Hospital.Name))))
+    ),
+    column(4,
+           selectInput("Address.1",
+                       "Address:",
+                       c("All",
+                         unique(as.character(data$Address.1))))
+    ),
+    column(4,
+           selectInput("Emergency.Services",
+                       "Emergency Services:",
+                       c("All",
+                         unique(as.character(data$Emergency.Services))))
+    )
+  ),
+  # Create a new row for the table.
+  DT::dataTableOutput("table")
 ))
